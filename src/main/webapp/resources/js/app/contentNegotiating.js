d7e677abc976399954605af16508b05c20ca25e9
sayu95 <jeongsayu95@gmail.com>
2025-12-04 03:12:17 +0900

    로케일과 타임존에 따른 시간 컨텐츠 제공

--- src/main/webapp/resources/js/app/contentNegotiating.js
+++ src/main/webapp/resources/js/app/contentNegotiating.js
@@ -1,16 +1,2 @@
-document.getElementById('localeSelector').addEventListener('change', (e) => {
-    const selectedLocaleCode = e.target.value;
-    
-    const params = new URLSearchParams();
-    
-    if (!selectedLocaleCode) {
-        document.getElementById('timezoneSelector').innerHTML = '<option value="">-- 먼저 언어를 선택하세요 --</option>';
-        return;
-    }
-    params.append('localeCode', selectedLocaleCode); 
-    
-    fetch('world-now', {
-        method: 'POST',
-        body: params
-    })
+document.addEventListener('DOMContentLoaded', function() {
 });
